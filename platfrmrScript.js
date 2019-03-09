ee100156f452cf3e346f254015679ecf9c83e534
Eric Zhou <42453465+TheZooo@users.noreply.github.com>
2019-03-09 13:16:16 -1000

    Redesign of X movement
    Gravity and Y movement later.

--- platfrmrScript.js
+++ platfrmrScript.js
@@ -2,108 +2,88 @@ var w = window.innerWidth; //Window Width
 var h = window.innerHeight; //Window Height
 var playerSize = 20; //Player Size
 var xSpeed = 0; //x speed that changes
-var xSpeedCap = 5; //x speed cap
-var xInc = 0;
-var xIncCap = 5;
-var xKeyActive = 0;
-var ySpeed = 0; //y speed that changes
-var ySpeedCap = 5; //y speed cap
-var yInc = 0;
-var yIncCap = 5;
-var yKeyActive = 0;
+var xSpeedCapMin = -5; //x speed cap MIN
+var xSpeedCapMax = 5; //x speed cap MAX
+var xInc = 0; //x increment in speed
+var xIncCap = 3; //cap when the increment happens
 
 var player = new player(); //Player object
 
 function setup() {
-  createCanvas(w, h);
+    createCanvas(w, h);
 }
 
 function draw() {
-  background(245);
-  stroke(100);
-  strokeWeight(6);
-  noFill();
-  rect(0, 0, w, h);
-  
-  player.display();
-  player.move();
-  
-  document.getElementById('dump').innerHTML = xSpeed + " " + xSpeedCap + " " + xInc + " keyactive: " + xKeyActive + " " + yKeyActive;
+    background(245);
+    stroke(100);
+    strokeWeight(6);
+    noFill();
+    rect(playerSize, playerSize, w - playerSize * 2, h - playerSize * 2);
+
+    player.display();
+    player.moveX();
+
+    document.getElementById('dump').innerHTML = xSpeed + " " + xInc;
 }
 
 function player() {
-  this.x = 10;
-  this.y = 10;
-  this.display = function () { //Displaying Player
-    stroke(0);
-    strokeWeight(1);
-    fill(48,136,36);
-    rect(this.x,this.y,playerSize,playerSize);
-  };
-  this.move = function () {
-    if (keyIsDown(65) || keyIsDown(68)) { //Increment xSpeed
-      if (xSpeed < xSpeedCap && xInc === xIncCap) {
-        xSpeed++;
-      } else if (xInc < xIncCap) {
-        xInc++;
-      }
-    } else if (xSpeed > 0 && xInc === 0) {
-      xSpeed--;
-      xInc = xIncCap;
-    } else if (xInc > 0) {
-      xInc--;
-    }
-    //----------------------------------------------------------
-    if (keyIsDown(87) || keyIsDown(83)) { //Increment ySpeed
-      if (ySpeed < ySpeedCap && yInc === yIncCap) {
-        ySpeed++;
-      } else if (yInc < yIncCap) {
-        yInc++;
-      }
-    } else if (ySpeed > 0 && yInc === 0) {
-      ySpeed--;
-      yInc = yIncCap;
-    } else if (yInc > 0) {
-      yInc--;
-    }
-    //----------------------------------------------------------
-    if (keyIsDown(65)) { //left
-      xKeyActive = 1;
-    }
-    if (keyIsDown(68)) { //right
-      xKeyActive = 2;
-    }
-    if (keyIsDown(87)) { //up
-      yKeyActive = 1;
-    }
-    if (keyIsDown(83)) { //down
-      yKeyActive = 2;
-    }
-    //----------------------------------------------------------
-    if (xKeyActive === 1) {
-      this.x -= xSpeed;
-    }
-    if (xKeyActive === 2) {
-      this.x += xSpeed;
-    }
-    if (yKeyActive === 1) {
-      this.y -= ySpeed;
-    }
-    if (yKeyActive === 2) {
-      this.y += ySpeed;
-    }
-    //----------------------------------------------------------
-    if (this.x > w - playerSize) {
-      this.x = w - playerSize;
-    }
-    if (this.x < 0) {
-      this.x = 0;
-    }
-    if (this.y > h - playerSize) {
-      this.y = h - playerSize;
-    }
-    if (this.y < 0) {
-      this.y = 0;
-    }
-  };
+    this.x = 40;
+    this.y = 40;
+    this.display = function () { //Displaying Player
+        stroke(0);
+        strokeWeight(1);
+        fill(48, 136, 36);
+        rect(this.x, this.y, playerSize, playerSize);
+        //--------------------------Borders--------------------------
+        if (this.x > w - playerSize * 2) {
+            this.x = w - playerSize * 2;
+            xSpeed = 0;
+        }
+        if (this.x < playerSize) {
+            this.x = playerSize;
+            xSpeed = 0;
+        }
+        if (this.y > h - playerSize * 2) {
+            this.y = h - playerSize * 2;
+            ySpeed = 0;
+        }
+        if (this.y < playerSize) {
+            this.y = playerSize;
+            ySpeed = 0;
+        }
+    };
+
+    this.moveX = function () { //Moving in terms of x position
+        //65 left, 68 right
+        if (keyIsDown(65) && !keyIsDown(68)) { //Increment xSpeed
+            if (xSpeed > xSpeedCapMin && xInc === xIncCap) {
+                xSpeed--;
+                xInc = 0;
+            } else if (xInc < xIncCap) {
+                xInc++;
+            }
+        }
+        if (keyIsDown(68) && !keyIsDown(65)) {
+            if (xSpeed < xSpeedCapMax && xInc === xIncCap) {
+                xSpeed++;
+                xInc = 0;
+            } else if (xInc < xIncCap) {
+                xInc++;
+            }
+        }
+        if ((!keyIsDown(65) && !keyIsDown(68)) || (keyIsDown(65) && keyIsDown(68))) {
+            if (xInc === 0) {
+                if (xSpeed > 0) {
+                    xSpeed--;
+                    xInc = xIncCap;
+                } else if (xSpeed < 0) {
+                    xSpeed++;
+                    xInc = xIncCap;
+                }
+            } else if (xInc > 0) {
+                xInc--;
+            }
+        }
+        this.x += xSpeed;
+    };
 }
